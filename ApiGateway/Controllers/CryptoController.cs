7645e62b94ee5dccaafe98fc1ff9c5318e51d3da
a.rudyi <a.rudyi@soft-industry.com>
2020-12-16 11:47:13 +0200

    Initial skeleton

---
+++ ApiGateway/Controllers/CryptoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoController : ControllerBase
+    {
+        private readonly ILogger<CryptoController> _logger;
+
+        public CryptoController(ILogger<CryptoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("encrypt")]
+        public string Encrypt(string secret)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("decrypt")]
+        public string Decrypt(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EncriptionService/Controllers/CryptoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EncriptionService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoController : ControllerBase
+    {
+        private readonly ILogger<CryptoController> _logger;
+
+        public CryptoController(ILogger<CryptoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("encrypt")]
+        public string Encrypt(string secret)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("decrypt")]
+        public string Decrypt(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("rotate")]
+        public void RotateEncryptionKey(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
